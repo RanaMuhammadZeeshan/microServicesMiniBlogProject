60004c844a18a1e37aa2ac1c49a912b333a04c94
zeeshan <rmzeeshan66@gmail.com>
2021-12-09 12:40:43 +0500

    creating and getting posts

---
+++ posts/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { randomBytes } = require('crypto')
+
+const app = express()
+app.use(bodyParser.json())
+
+//store posts in memory
+const posts = {}
+
+app.get('/posts', (req, res) => {
+  res.send(posts)
+})
+
+app.post('/posts', (req, res) => {
+  //generating random id using randomBytes
+  const id = randomBytes(4).toString('hex')
+
+  const { title } = req.body
+
+  //adding key (id) and value (title) to posts object
+  posts[id] = {
+    id,
+    title,
+  }
+
+  res.status(201).send(posts[id])
+})
+
+app.listen(4000, () => {
+  console.log('listening on 4000')
+})
