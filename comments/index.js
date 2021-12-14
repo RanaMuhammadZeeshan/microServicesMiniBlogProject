de2250472f64305b5c96e6c186259c700d33628b
zeeshan <rmzeeshan66@gmail.com>
2021-12-14 11:12:03 +0500

    event-bus created for postcreated and commentcreated

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { randomBytes } = require('crypto')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
@@ -14,7 +15,7 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || [])
 })
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   //generate a random id for the comment
   const commentId = randomBytes(4).toString('hex')
 
@@ -27,7 +28,23 @@ app.post('/posts/:id/comments', (req, res) => {
 
   commentsByPostId[req.params.id] = comments
 
+  //triggering an event for event-bus
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  })
+
   res.status(201).send(comments)
+})
+
+app.post('/events', (req, res) => {
+  console.log('Revicieved an event', req.body.type)
+
+  res.send({})
 })
 
 app.listen(4001, () => {
